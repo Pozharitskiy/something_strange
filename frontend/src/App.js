441e15de7abff79a85b427c7c38d7c59fc5d1e89
Pozharitskiy <pozharitskij.denis@gmail.com>
2020-06-01 00:43:25 +0300

    auth bug fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
+//prettier-ignore
+import React from "react";
+import { connect } from "react-redux";
 
-import './App.css';
+import "./App.css";
 
-const App = () => (
-  <div className="App">
-    app
-  </div>
-)
+const App = ({ state }) => {
+  console.log(state);
+  return <div className="App">app</div>;
+};
 
-const mapStateToProps = state => (
-  state
-  //state
-)
+const mapStateToProps = (state) => state;
+//state
 
 // const mapDispatchToProps = (dispatch) => {
 //   //functions
 // }
-
-
 
 export default connect(mapStateToProps)(App);
--- frontend/src/reducers/authReducers.js
+++ frontend/src/reducers/authReducers.js
@@ -1,6 +1,4 @@
-import { SET_AUTH_DATA, SET_TOKEN, LOGIN_USER } from "../actions/actions";
-import createToken from "../utils/createToken";
-import http from "../services/http";
+import { SET_TOKEN } from "../actions/actions";
 
 const initialState = {
   userName: "",
@@ -11,14 +9,6 @@ const initialState = {
 
 const authReducers = (state = initialState, action) => {
   switch (action.type) {
-    // case SET_AUTH_DATA:
-    //   // const token = createToken();
-    //   const userData = { ...action.data, token: token };
-
-    //   // localStorage.setItem("token", token);
-
-    //   return { ...state, ...userData };
-
     case SET_TOKEN:
       localStorage.setItem("token", action.token);
       return { ...state, token: action.token };
